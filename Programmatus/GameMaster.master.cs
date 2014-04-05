d8bb9ccab50fecb5532ff1d60fcc4e4dbc39cfdf
Adrian Tollisen <cxyadrian@gmail.com>
2014-04-05 18:25:01 +0200

    Lagt til noe i train skills knappen, i Mastergame.cs fila har jeg lagt til comments for å hvise hvor ting starter og hvor ting slutter, og train skills synlig og ikke synnlig

--- Programmatus/GameMaster.master.cs
+++ Programmatus/GameMaster.master.cs
@@ -28,52 +28,101 @@ public partial class GameMaster : System.Web.UI.MasterPage
         reader.Close();
         con.Close();*/
     }
+    //Overview -------------------------------------------------------------------------------------------------
     protected void Overview_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = false;
+
         Job1.Visible = false;
         Job2.Visible = false;
         Job3.Visible = false; 
     }
+    //Overview END -------------------------------------------------------------------------------------------
+
+    //Market -----------------------------------------------------------------------------------------------------
     protected void Market_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = false;
+
         Job1.Visible = false;
         Job2.Visible = false;
         Job3.Visible = false;
     }
+    //Market END --------------------------------------------------------------------------------------------------
+
+    //Train Skills -----------------------------------------------------------------------------------------------
     protected void Skills_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = true;
         Job1.Visible = false;
         Job2.Visible = false;
         Job3.Visible = false;
     }
+    protected void TrainEfficiency_clicked(object sender, EventArgs e)
+    {
+        Job1.Visible = false;
+        Job2.Visible = false;
+        Job3.Visible = false;
+    }
+    protected void TrainSecurity_clicked(object sender, EventArgs e)
+    {
+        Job1.Visible = false;
+        Job2.Visible = false;
+        Job3.Visible = false;
+    }
+    protected void TrainBargaining_clicked(object sender, EventArgs e)
+    {
+        Job1.Visible = false;
+        Job2.Visible = false;
+        Job3.Visible = false;
+    }
+    //Train Skills END ----------------------------------------------------------------------------------------------
+
+    //JOB ----------------------------------------------------------------------------------------------------------
     protected void Job_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = false;
+
         Job1.Visible = true;
         Job2.Visible = true;
         Job3.Visible = true;
     }
-    protected void Hack_clicked(object sender, EventArgs e)
-    {
-        Job1.Visible = false;
-        Job2.Visible = false;
-        Job3.Visible = false;
-    }
+    
     protected void Job1_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = false;
+
         Job1.Visible = false;
         Job2.Visible = false;
         Job3.Visible = false;
     }
     protected void Job2_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = false;
+
         Job1.Visible = false;
         Job2.Visible = false;
         Job3.Visible = false;
     }
     protected void Job3_clicked(object sender, EventArgs e)
     {
+        TrainSkills.Visible = false;
+
         Job1.Visible = false;
         Job2.Visible = false;
         Job3.Visible = false;
     }
+    //JOB END ---------------------------------------------------------------------------------------------------
+
+    //Hack ------------------------------------------------------------------------------------------------------
+    protected void Hack_clicked(object sender, EventArgs e)
+    {
+        TrainSkills.Visible = false;
+
+        Job1.Visible = false;
+        Job2.Visible = false;
+        Job3.Visible = false;
+    }
+    //Hack END -------------------------------------------------------------------------------------------------
+    
 }
